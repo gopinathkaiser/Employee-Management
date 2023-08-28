4652a8e3fbd9d7544349ee5ee323b06c327ed823
gopinath <gopinathkaiser@gmail.com>
2023-08-28 15:49:53 +0530

    first commit

---
+++ src/test/java/com/example/fullstackapplication/FullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullstackapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
